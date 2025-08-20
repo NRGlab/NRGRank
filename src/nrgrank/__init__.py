0541725f2e98c732430feb8e99d8f08861feaf03
Thomas DesCoteaux <thomas.descoteaux@umontreal.ca>
2025-08-19 23:14:45 -0600

    remove rdkit dependency

--- src/nrgrank/__init__.py
+++ src/nrgrank/__init__.py
@@ -1,4 +1,3 @@
 from .process_target import main as process_target
 from .process_ligands import main as process_ligands
 from .rank_molecules import main as nrgrank_main
-from .generate_conformers import main as generate_conformers
